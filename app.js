00e363433980ede9c9b4fd357c24e355aef57ac4
Arsh <arshadjamal1996@gmail.com>
2023-07-15 23:27:52 +0530

    Render dynamic data

--- app.js
+++ app.js
@@ -1,51 +1,33 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-var today = new Date();
-var currentDay = today.getDay();
-var day = "";
+var items = [];
 
 app.get('/', function(req, res){
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-        break;
-            
-        case 1:
-            day = "Monday";
-        break;
+    var today = new Date();
+    var options = {
+        weekday : "long",
+        day: "numeric",
+        month: "long"
+    };
 
-        case 2:
-            day = "Tuesday";
-        break;
-            
-        case 3:
-            day = "Wednesday";
-        break;
+    var day = today.toLocaleDateString("en-US", options);
 
-        case 4:
-            day = "Thursady";
-        break;
+    res.render("list", {kindOfDay : day, newListItems : items});
 
-        case 5:
-            day = "Friday";
-        break;
-            
-        case 6:
-            day = "Saturday";
-        break;
-    
-        default:
-            console.log("Error: current day is " + currentDay);
-        break;
-    }
+});
 
-    res.render("list", {kindOfDay : day});
-
+app.post('/', function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
